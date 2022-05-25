f2d5a13d82d7392a782550b5176a93ff3302f860
charmleesb <charmleesb@gmail.com>
2022-05-25 23:09:18 +0900

    Update 구현

--- RestDemo/index.js
+++ RestDemo/index.js
@@ -1,27 +1,36 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+const methodOverride = require('method-override')
+const { v4: uuid } = require('uuid');
+uuid();
+
 // request body에서 URL 암호화 데이터(urlencoded) 형태 읽기
 app.use(express.urlencoded({ extended: true }))
 // json 데이터 형태 읽기
 app.use(express.json());
+app.use(methodOverride('_method'))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
 const comments = [
     {
+        id: uuid(),
         username: 'Todd',
         comment: 'lol that is so funny!'
     },
     {
+        id: uuid(),
         username: 'Skyler',
         comment: 'I like to go birdwatiching with my dog'
     },
     {
+        id: uuid(),
         username: 'Sk8erBoi',
         comment: 'Plz delete your account, Todd'
     },
     {
+        id: uuid(),
         username: 'onlysayswoof',
         comment: 'woof'
     }
@@ -37,8 +46,28 @@ app.get('/comments/new', (req, res) => {
 
 app.post('/comments', (req, res) => {
     const { username, comment } = req.body;
-    comments.push({ username, comment });
-    res.send("IT WORKED!");
+    comments.push({ username, comment, id: uuid() });
+    res.redirect('/comments');
+})
+
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/show', { comment })
+})
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment })
+})
+
+app.patch('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const newCommentText = req.body.comment;
+    const foundComment = comments.find(c => c.id === id);
+    foundComment.comment = newCommentText;
+    res.redirect('/comments');
 })
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response")
