8703b127bd0943bc00179b1b6aea6e9ef7af6219
charmleesb <charmleesb@gmail.com>
2022-05-31 21:35:31 +0900

    DELETE 구현

--- RestDemo/index.js
+++ RestDemo/index.js
@@ -13,7 +13,7 @@ app.use(methodOverride('_method'))
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'))
 
-const comments = [
+let comments = [
     {
         id: uuid(),
         username: 'Todd',
@@ -69,6 +69,13 @@ app.patch('/comments/:id', (req, res) => {
     foundComment.comment = newCommentText;
     res.redirect('/comments');
 })
+
+app.delete('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    comments = comments.filter(c => c.id !== id);
+    res.redirect('/comments');
+})
+
 app.get('/tacos', (req, res) => {
     res.send("GET /tacos response")
 })
